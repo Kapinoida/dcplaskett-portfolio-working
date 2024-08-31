9ec9a4588175fea5031e90cdb255601b571a389a
David Plaskett <dcplaskett@gmail.com>
2024-08-31 11:33:46 -0500

    fixed error

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
     return (
         <div className="flex min-h-screen flex-col items-center justify-center">
             <h1 className="md:text-4xl text-3xl py-4">404.</h1>
-            <p>This is not the page you're looking for.</p>
+            <p>This is not the page you are looking for.</p>
         </div>
     );
 }
