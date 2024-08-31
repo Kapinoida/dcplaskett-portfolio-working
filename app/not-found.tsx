b2da31f3f6ea919063d9512e5144de41cf0839ca
David Plaskett <dcplaskett@gmail.com>
2024-08-31 11:18:05 -0500

    Added custom not-found page.

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+
+
+export default function NotFound() {
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-center">
+            <h1 className="md:text-4xl text-3xl py-4">404.</h1>
+            <p>This is not the page you're looking for.</p>
+        </div>
+    );
+}
